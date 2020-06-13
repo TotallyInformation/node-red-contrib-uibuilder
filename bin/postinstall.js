7a6597facdcc8a8ef6f7512c67fdd269b9884f66
Julian Knight <jk.github@totallyinformation.com>
2020-06-13 17:38:02 +0100

    trying to get pre-install working properly

--- bin/postinstall.js
+++
@@ -1,5 +0,0 @@
-/** Display any post installation messages */
-console.log(`
-  NOTE that you can ignore warnings from bootstrap about missing jQuery.
-       Unless you are using bootstrap directly, bootstrap-vue takes care of everything.
-`)
