f4b195b6eef15b91e30c786e68f4201f83e1cc0c
Julian Knight <1591850+TotallyInformation@users.noreply.github.com>
2021-02-16 15:12:20 +0000

    New zero dependency template

---
+++ templates/blank/README.md
@@ -0,0 +1,9 @@
+# uibuild Template folder: Blank
+
+This is about the simplest template you can get for uibuilder.
+
+It does not use any frameworks and has no other dependencies.
+
+It demonstrates that you can use uibuilder purely with HTML/JavaScript.
+
+All it does is start up uibuilder and will dump any msg you send to it from Node-RED into the browser tab.
---
+++ templates/blank/index.js
@@ -0,0 +1,40 @@
+/* jshint browser: true, esversion: 5, asi: true */
+/* globals uibuilder */
+// @ts-nocheck
+
+// return formatted HTML version of JSON object
+function syntaxHighlight(json) {
+    json = JSON.stringify(json, undefined, 4)
+    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;')
+    json = json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+        var cls = 'number'
+        if (/^"/.test(match)) {
+            if (/:$/.test(match)) {
+                cls = 'key'
+            } else {
+                cls = 'string'
+            }
+        } else if (/true|false/.test(match)) {
+            cls = 'boolean'
+        } else if (/null/.test(match)) {
+            cls = 'null'
+        }
+        return '<span class="' + cls + '">' + match + '</span>'
+    })
+    return json
+} // --- End of syntaxHighlight --- //
+
+// run this function when the document is loaded
+window.onload = function() {
+    // Start up uibuilder - see the docs for the optional parameters
+    uibuilder.start()
+
+    // Listen for incoming messages from Node-RED
+    uibuilder.onChange('msg', function(msg){
+        console.info('[indexjs:uibuilder.onChange] msg received from Node-RED server:', msg)
+
+        // dump the msg as text to the "msg" html element
+        const eMsg = document.getElementById('msg')
+        eMsg.innerHTML = syntaxHighlight(msg)
+    })
+}
