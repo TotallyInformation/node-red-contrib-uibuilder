f6175c39156498ffe84561f1ab6d66317e61a01f
Julian Knight <1591850+TotallyInformation@users.noreply.github.com>
2021-06-24 14:11:11 +0100

    v4

--- front-end/src/uibuilderfe.js
+++ front-end/src/uibuilderfe.js
@@ -74,7 +74,7 @@ if (typeof require !== 'undefined'  &&  typeof io === 'undefined') {
 
         //#region ======== Start of setup ======== //
 
-        self.version = '3.3.0'
+        self.version = '4.0.0'
         self.debug = false // do not change directly - use .debug() method
         self.moduleName  = 'uibuilder' // Must match moduleName in uibuilder.js on the server
         // @ts-expect-error ts(2345) Tests loaded ver of lib to see if minified 
