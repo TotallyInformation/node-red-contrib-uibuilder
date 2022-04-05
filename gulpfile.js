feae5010f9e6149a83fb3d1e436f1b81c11fe7f5
Julian Knight <1591850+TotallyInformation@users.noreply.github.com>
2022-04-05 01:19:38 +0100

    Bump to v5.0.1

--- gulpfile.js
+++ gulpfile.js
@@ -58,7 +58,7 @@ const stdio = 'inherit'
 const { version } = JSON.parse(fs.readFileSync('package.json'))
 
 //npm version 4.2.1 --no-git-tag-version --allow-same-version
-const release = '5.0.0'
+const release = '5.0.1'
 
 console.log(`Current Version: ${version}. Requested Version: ${release}`)
 
