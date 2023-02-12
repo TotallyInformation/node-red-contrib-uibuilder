b59e3c22d914fa3de1f392f24fd4400ee6337429
Julian Knight <1591850+TotallyInformation@users.noreply.github.com>
2023-02-12 17:33:40 +0000

    Extended example

--- templates/iife-blank-client/src/index.js
+++ templates/iife-blank-client/src/index.js
@@ -1,5 +1,4 @@
 // @ts-nocheck
-
 /** Example of using the IIFE build of the uibuilder client library
  * Note that uibuilder.start() should no longer be needed.
  * See the docs if the client doesn't start on its own.
@@ -7,10 +6,15 @@
 'use strict'
 
 // logLevel 2+ shows more built-in logging. 0=error,1=warn,2=info,3=log,4=debug,5=trace.
-// uibuilder.set('logLevel', 2)
+// uibuilder.set('logLevel', 2) // uibuilder.set('logLevel', 'info')
+// Using the log yourself:
 // uibuilder.log('info', 'a prefix', 'some info', {any:'data',life:42})
 
-// Helper function to send a message back to Node-RED using the standard send function - see the HTML file for use
+// Show the latest incoming msg from Node-RED
+uibuilder.showMsg(true, 'body')
+
+// Helper function to send a message back to Node-RED using the standard send function
+// - see the HTML file for use. Can, of course, add any custom data in the msg.
 window.fnSendToNR = function fnSendToNR(payload) {
     uibuilder.send({
         'topic': 'msg-from-uibuilder-front-end',
@@ -18,11 +22,7 @@ window.fnSendToNR = function fnSendToNR(payload) {
     })
 }
 
-// Listen for incoming messages from Node-RED
-uibuilder.onChange('msg', function(msg) {
-    // Dump the msg as text to the "msg" html element
-    // either the HTML way or via uibuilder's $ helper function
-    // const eMsg = document.getElementById('msg')
-    const eMsg = $('#msg')
-    if (eMsg) eMsg.innerHTML = uibuilder.syntaxHighlight(msg)
-})
+// Listen for incoming messages from Node-RED and action
+// uibuilder.onChange('msg', function(msg) {
+//     // do stuff with the incoming msg
+// })
