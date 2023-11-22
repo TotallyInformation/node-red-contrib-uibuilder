9f9ca1ef07f0d13f03cba01d80b6548616da1dbf
Julian Knight <1591850+TotallyInformation@users.noreply.github.com>
2023-11-22 17:58:24 +0000

    Set all versions to v6.7.0

--- gulpfile.js
+++ gulpfile.js
@@ -71,7 +71,7 @@ const stdio = 'inherit'
 // @ts-ignore Find the module version in the package.json
 const { version } = JSON.parse(fs.readFileSync('package.json'))
 // Desired release version
-const release = '6.6.0'
+const release = '6.7.0'
 // Wanted node.js version - used for ESBUILD
 const nodeVersion = 'node14.14'
 
