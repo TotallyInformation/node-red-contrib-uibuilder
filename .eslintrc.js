228e720c5682cee5918335fb34117181e18cbd50
Julian Knight <1591850+TotallyInformation@users.noreply.github.com>
2024-09-02 21:06:47 +0100

    Update eslint settings to v8, combine sub-folder settings to overrides

--- .eslintrc.js
+++ .eslintrc.js
@@ -31,31 +31,26 @@
  *       Intl.supportedValuesOf function
  * v19 - (Experimental: node --watch), HTTP(S)/1.1 KeepAlive by default, Stable WebCrypto API, Intl.NumberFormat
  */
+
 module.exports = {
+    root: true,
     env: {
         browser: false,
         commonjs: true,
+        es2022: true,
         jquery: false,
         node: true,
-        // es2019: true,
-        'shared-node-browser': false
+        'shared-node-browser': false,
     },
-    parserOptions: {
-        // Only ESLint 6.2.0 and later support ES2020. Node.js v12+ supports some things only ratified in 2020
-        'ecmaVersion': 2022,
-        sourceType: 'script'
-    },
-    root: true,
     globals: {
         Set: true, // Not sure why eslint doesn't recognise this as it is part of node.js since v0.12
         RED: true,
     },
-    overrides: [
-        {
-            files: ['*.module.js', '*.mod.js', '*.mjs'],
-            parserOptions: { sourceType: 'module' },
-        }
-    ],
+    parserOptions: {
+        // Only ESLint 6.2.0 and later support ES2020. Node.js v12+ supports some things only ratified in 2020
+        // ecmaVersion: 2022,
+        sourceType: 'script'
+    },
     plugins: [
         'html',     // Check scripts in HTML. https://www.npmjs.com/package/eslint-plugin-html
         'es',       // Help avoid js that is too new. https://eslint-plugin-es.mysticatea.dev/
@@ -66,24 +61,142 @@ module.exports = {
         // 'eslint-plugin-n', // loads itself from extends, no need to manually load
     ],
     extends: [
-        'standard',
+        'standard', // Also loads the n plugin
         // 'eslint:recommended',
         'plugin:es/no-new-in-esnext',
         'plugin:jsdoc/recommended',
         'plugin:promise/recommended',
-        'plugin:sonarjs/recommended',
+        'plugin:sonarjs/recommended-legacy', // NB: Needs changing for eslint v9. https://www.npmjs.com/package/eslint-plugin-sonarjs#usage
         // 'plugin:prettier/recommended',
         // The n plugin reads the min. node.js version from package.json and error's any ES features not available in that version.
-        // https://www.npmjs.com/package/eslint-plugin-n#-rules
         'plugin:n/recommended',
     ],
-    // settings: {
-    //     jsdoc: {
-    //         mode: 'permissive'
-    //     }
-    // },
+    overrides: [
+        { // Override for Node-RED Editor js & html files
+            env: {
+                browser: true,
+                es2019: true,
+                es2022: false,
+                jquery: true,
+                node: false,
+            },
+            files: [
+                'resources/*.js',
+                'nodes/**/*.html',
+                'src/editor/**/*.{js,html}',
+            ],
+            parserOptions: {
+                sourceType: 'script',
+            },
+            rules: {
+                // remove once min engines moves to node.js v14+
+                'es/no-optional-chaining': 'error',
+                'es/no-dynamic-import': 'error',
+                'es/no-nullish-coalescing-operators': 'error',
+            }
+        },
+        { // Override for front-end & docs std js & html files
+            env: {
+                browser: true,
+                es2019: true,
+                es2022: false,
+                jquery: false,
+                node: false,
+            },
+            files: [
+                'front-end/**/*.js',
+                'front-end/**/*.html',
+                'docs/**/*.js',
+                'docs/**/*.html',
+            ],
+            excludedFiles: [
+                '*.{esm.js,esm.min.js}',
+            ],
+            parserOptions: {
+                sourceType: 'script',
+            },
+            rules: {
+                // remove once min engines moves to node.js v14+
+                'es/no-optional-chaining': 'error',
+                'es/no-dynamic-import': 'error',
+                'es/no-nullish-coalescing-operators': 'error',
+            }
+        },
+        { // Override for front-end esm js & html files
+            env: {
+                browser: true,
+                es2019: false,
+                es2022: true,
+                jquery: false,
+                node: false,
+            },
+            files: [
+                'front-end/**/*.esm.js',
+                'front-end/**/*.esm.min.js',
+            ],
+            excludedFiles: [
+                '*.{esm.js,esm.min.js}',
+            ],
+            parserOptions: {
+                sourceType: 'module',
+            },
+            rules: {
+                // remove once min engines moves to node.js v14+
+                'es/no-optional-chaining': 'error',
+                'es/no-dynamic-import': 'error',
+                'es/no-nullish-coalescing-operators': 'error',
+            }
+        },
+        { // Override for src esm js files
+            env: {
+                browser: true,
+                es2019: false,
+                es2022: true,
+                jquery: false,
+                node: false,
+            },
+            files: [
+                'src/components/*.js',
+                'src/front-end-module/*.js',
+            ],
+            parserOptions: {
+                sourceType: 'module',
+            },
+        },
+        { // Override for .eslintrc.js/cjs files
+            env: {
+                browser: false,
+                commonjs: true,
+                es2022: true,
+                jquery: false,
+                node: true,
+            },
+            files: [
+                '.eslintrc.{js,cjs}',
+            ],
+            parserOptions: {
+                sourceType: 'script',
+            }
+        },
+        { // Override for ESM files
+            files: ['*.module.js', '*.mod.js', '*.mjs'],
+            parserOptions: {
+                sourceType: 'module',
+            },
+        },
+    ],
     rules: {
         'n/no-process-exit': 'error',
+
+        // remove once min engines moves to node.js v14+
+        // 'es/no-optional-chaining': 'error',
+        // 'es/no-dynamic-import': 'error',
+        // 'es/no-nullish-coalescing-operators': 'error',
+
+        // remove once min engines moves to node.js v15+
+        'es/no-logical-assignment-operators': 'error',
+        'es/no-promise-any': 'error',
+        'es/no-numeric-separators': 'error',
 
         // Tidy up some jsdoc oddities
         'jsdoc/multiline-blocks': 0,
@@ -113,305 +226,3 @@ module.exports = {
         'quote-props': 'off',     // Sometimes it is necessary and then much nicer to be able to quote things that don't need it.
     }
 }
-
-/*
-        quotes: [
-            'warn',
-            'single'
-        ],
-
-        'accessor-pairs': 'error',
-        'array-bracket-newline': 'error',
-        'array-bracket-spacing': 0,
-        'array-callback-return': 'off',
-        'array-element-newline': 'off',
-        'arrow-body-style': 'error',
-        'arrow-parens': 'off',
-        'arrow-spacing': [
-            'error',
-            {
-                after: true,
-                before: true
-            }
-        ],
-        'block-scoped-var': 'error',
-        'block-spacing': [
-            'error',
-            'always'
-        ],
-        'brace-style': [
-            'error',
-            '1tbs',
-            {
-                allowSingleLine: true
-            }
-        ],
-        'callback-return': 'error',
-        camelcase: 'off',
-        'capitalized-comments': 'off',
-        'class-methods-use-this': 'error',
-        'comma-dangle': 'off',
-        'comma-spacing': 'off',
-        'comma-style': [
-            'error',
-            'last'
-        ],
-        complexity: 'off',
-        'computed-property-spacing': [
-            'error',
-            'never'
-        ],
-        'consistent-return': 'off',
-        'consistent-this': 'off',
-        curly: 'off',
-        'default-case': 'off',
-        'default-case-last': 'error',
-        'default-param-last': 'error',
-        'dot-location': [
-            'error',
-            'property'
-        ],
-        'dot-notation': [
-            'error',
-            {
-                allowKeywords: true
-            }
-        ],
-        'eol-last': 'error',
-        eqeqeq: 'error',
-        'func-call-spacing': 'error',
-        'func-name-matching': 'error',
-        'func-names': 'off',
-        'func-style': 'off',
-        'function-call-argument-newline': 'off',
-        'function-paren-newline': 'off',
-        'generator-star-spacing': 'error',
-        'global-require': 'off',
-        'grouped-accessor-pairs': 'error',
-        'guard-for-in': 'error',
-        'handle-callback-err': 'error',
-        'id-blacklist': 'error',
-        'id-denylist': 'error',
-        'id-length': 'off',
-        'id-match': 'error',
-        'implicit-arrow-linebreak': [
-            'error',
-            'beside'
-        ],
-        indent: [
-            'warn',
-            4,
-            { 'SwitchCase': 1 }
-        ],
-        'indent-legacy': 'off',
-        'init-declarations': 'off',
-        'jsx-quotes': 'error',
-        'key-spacing': 'off',
-        'keyword-spacing': 'off',
-        'line-comment-position': 'off',
-        'linebreak-style': [
-            'error',
-            'unix'
-        ],
-        'lines-around-comment': 'off',
-        'lines-around-directive': 'off',
-        'lines-between-class-members': 'error',
-        'max-classes-per-file': 'error',
-        'max-depth': 'error',
-        'max-len': 'off',
-        'max-lines': 'off',
-        'max-lines-per-function': 'off',
-        'max-nested-callbacks': 'error',
-        'max-params': ['error', 6],
-        'max-statements': 'off',
-        'max-statements-per-line': 'off',
-        'multiline-comment-style': 'off',
-        'new-cap': 'error',
-        'new-parens': 'error',
-        'newline-after-var': 'off',
-        'newline-before-return': 'off',
-        'newline-per-chained-call': 'error',
-        'no-alert': 'error',
-        'no-array-constructor': 'error',
-        'no-await-in-loop': 'error',
-        'no-bitwise': 'off',
-        'no-buffer-constructor': 'error',
-        'no-caller': 'error',
-        'no-catch-shadow': 'error',
-        'no-confusing-arrow': 'error',
-        'no-console': 'off',
-        'no-constructor-return': 'error',
-        'no-continue': 'error',
-        'no-div-regex': 'error',
-        'no-duplicate-imports': 'error',
-        'no-else-return': [
-            'error',
-            {
-                allowElseIf: true
-            }
-        ],
-        'no-empty-function': 'off',
-        'no-eq-null': 'error',
-        'no-eval': 'error',
-        'no-extend-native': 'error',
-        'no-extra-bind': 'error',
-        'no-extra-label': 'error',
-        'no-extra-parens': 'off',
-        'no-floating-decimal': 'error',
-        'no-implicit-coercion': 'error',
-        'no-implicit-globals': 'error',
-        'no-implied-eval': 'error',
-        'no-inline-comments': 'off',
-        'no-inner-declarations': [
-            'error',
-            'functions'
-        ],
-        'no-invalid-this': 'off',
-        'no-iterator': 'error',
-        'no-label-var': 'error',
-        'no-labels': 'error',
-        'no-lone-blocks': 'error',
-        'no-lonely-if': 'error',
-        'no-loop-func': 'error',
-        'no-loss-of-precision': 'error',
-        'no-magic-numbers': 'off',
-        'no-mixed-operators': 'error',
-        'no-mixed-requires': 'error',
-        'no-multi-assign': 'off',
-        'no-multi-spaces': 'off',
-        'no-multi-str': 'error',
-        'no-multiple-empty-lines': 'error',
-        'no-native-reassign': 'error',
-        'no-negated-condition': 'off',
-        'no-negated-in-lhs': 'error',
-        'no-nested-ternary': 'error',
-        'no-new': 'error',
-        'no-new-func': 'error',
-        'no-new-object': 'error',
-        'no-new-require': 'error',
-        'no-new-wrappers': 'error',
-        'no-nonoctal-decimal-escape': 'error',
-        'no-octal-escape': 'error',
-        'no-param-reassign': 'off',
-        'no-path-concat': 'error',
-        'no-plusplus': 'off',
-        'no-process-env': 'off',
-        'no-process-exit': 'error',
-        'no-promise-executor-return': 'error',
-        'no-proto': 'error',
-        'no-restricted-exports': 'error',
-        'no-restricted-globals': 'error',
-        'no-restricted-imports': 'error',
-        'no-restricted-modules': 'error',
-        'no-restricted-properties': 'error',
-        'no-restricted-syntax': 'error',
-        'no-return-assign': 'error',
-        'no-return-await': 'error',
-        'no-script-url': 'error',
-        'no-self-compare': 'error',
-        'no-sequences': 'error',
-        'no-shadow': 'off',
-        'no-spaced-func': 'error',
-        'no-sync': 'off',
-        'no-tabs': 'error',
-        'no-template-curly-in-string': 'error',
-        'no-ternary': 'off',
-        'no-throw-literal': 'error',
-        'no-trailing-spaces': 'off',
-        'no-undef-init': 'error',
-        'no-undefined': 'off',
-        'no-underscore-dangle': 'off',
-        'no-unmodified-loop-condition': 'error',
-        'no-unneeded-ternary': 'off',
-        'no-unreachable-loop': 'error',
-        'no-unsafe-optional-chaining': 'error',
-        'no-use-before-define': 'error',
-        'no-useless-backreference': 'error',
-        'no-useless-call': 'error',
-        'no-useless-computed-key': 'error',
-        'no-useless-concat': 'error',
-        'no-useless-constructor': 'error',
-        'no-useless-rename': 'error',
-        'no-useless-return': 'off',
-        'no-var': 'off',
-        'no-void': 'error',
-        'no-warning-comments': 'off',
-        'no-whitespace-before-property': 'error',
-        'nonblock-statement-body-position': [
-            'error',
-            'any'
-        ],
-        'object-curly-newline': 'error',
-        'object-curly-spacing': 'off',
-        'object-shorthand': 'off',
-        'one-var': 'off',
-        'one-var-declaration-per-line': 'off',
-        'operator-assignment': [
-            'error',
-            'always'
-        ],
-        'operator-linebreak': 'error',
-        'padded-blocks': 'off',
-        'padding-line-between-statements': 'error',
-        'prefer-arrow-callback': 'off',
-        'prefer-const': 'off',
-        'prefer-destructuring': 'off',
-        'prefer-exponentiation-operator': 'error',
-        'prefer-named-capture-group': 'error',
-        'prefer-numeric-literals': 'error',
-        'prefer-object-spread': 'off',
-        'prefer-promise-reject-errors': 'error',
-        'prefer-reflect': 'off',
-        'prefer-regex-literals': 'error',
-        'prefer-rest-params': 'off',
-        'prefer-spread': 'off',
-        'prefer-template': 'off',
-        'quote-props': 'off',
-        radix: 'error',
-        'require-atomic-updates': 'error',
-        'require-await': 'error',
-        'require-jsdoc': 'error',
-        'require-unicode-regexp': 'off',
-        'rest-spread-spacing': 'error',
-        semi: [
-            'warn',
-            'never'
-        ],
-        'semi-spacing': 'error',
-        'semi-style': 'error',
-        'sort-imports': 'error',
-        'sort-keys': 'off',
-        'sort-vars': 'off',
-        'space-before-blocks': 'off',
-        'space-before-function-paren': 'off',
-        'space-in-parens': 'off',
-        'space-infix-ops': 'off',
-        'space-unary-ops': 'off',
-        'spaced-comment': 'off',
-        strict: 'error',
-        'switch-colon-spacing': [
-            'error',
-            {
-                after: true,
-                before: false
-            }
-        ],
-        'symbol-description': 'error',
-        'template-curly-spacing': 0,
-        'template-tag-spacing': 'error',
-        'unicode-bom': [
-            'error',
-            'never'
-        ],
-        'valid-jsdoc': 'off',
-        'vars-on-top': 'off',
-        'wrap-iife': 'error',
-        'wrap-regex': 'error',
-        'yield-star-spacing': 'error',
-        yoda: [
-            'error',
-            'never'
-        ]
-    },
-}
-*/
