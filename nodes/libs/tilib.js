063019769519277d925140c2c7c268c5a4e1ae45
Julian Knight <1591850+TotallyInformation@users.noreply.github.com>
2024-09-23 15:28:42 +0100

    Add truthy fn

--- nodes/libs/tilib.js
+++ nodes/libs/tilib.js
@@ -133,6 +133,19 @@ module.exports = {
         return json
     }, // ----  ---- //
 
+    /** Returns true/false or a default value for truthy/falsy and other values
+     * @param {string|number|boolean|*} val The value to test
+     * @param {any} deflt Default value to use if the value is not truthy/falsy
+     * @returns {boolean|any} The truth! Or the default
+     */
+    truthy(val, deflt) {
+        let ret
+        if (['on', 'On', 'ON', 'true', 'True', 'TRUE', '1', true, 1].includes(val)) ret = true
+        else if (['off', 'Off', 'OFF', 'false', 'False', 'FALSE', '0', false, 0].includes(val)) ret = false
+        else ret = deflt
+        return ret
+    },
+
     /** Compare 2 simple arrays, return array of arrays - additions and deletions
      * @param {Array} a1 First array
      * @param {Array} a2 Second array
