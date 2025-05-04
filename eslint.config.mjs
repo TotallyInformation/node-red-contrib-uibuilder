cdbde50e43a275bc48babb7922e0ad7b7085b31f
Julian Knight <1591850+TotallyInformation@users.noreply.github.com>
2025-05-04 16:51:38 +0100

    Yet another attempt at getting the eslint v9 config to work! What an absolute pain. :-(

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,7 @@
  * npx eslint --print-config file.js
  */
 
+import { defineConfig } from 'eslint/config'
 import globals from 'globals' // https://www.npmjs.com/package/globals
 // @ts-ignore
 import pluginImport from 'eslint-plugin-import' // https://www.npmjs.com/package/eslint-plugin-import
@@ -20,236 +21,179 @@ import node from 'eslint-plugin-n' // https://www.npmjs.com/package/eslint-plugi
 import stylistic from '@stylistic/eslint-plugin' // https://eslint.style
 import js from '@eslint/js'
 
-// Folder/file lists
-const browserCJS = [ // browser, es2019
-    // User FE code, mostly CJS/IIFE
-    'front-end/**/*.iife.js',
-    'front-end/**/*.html',
-    // Documentation
-    'docs/**/*.js',
-    'docs/**/*.html',
-    // NR Editor FE code
-    'resources/*.js', // these are actually being moved to load as modules but are not built so need es2019
-]
-const browserMJS = [ // browser, latest, module
-    // Built-in web components source code - Use ESBUILD, treat as ESM
-    'src/components/*.js',
-    // Source code for the FE libraries - Use ESBUILD, treat as ESM
-    'src/front-end-module/*.js',
-    // User FE code, ESM versions - already built so not worried about es version
-    'front-end/**/*.esm.js',
-]
-const nodeCJS = [ // node, node18, cjs
-    'nodes/elements/elementBuilder.js',
-    // NR runtime code
-    // 'nodes/libs/*.js',
-    'nodes/**/*.{js|cjs}',
-    // Update the docs bundle for docsify offline use
-    'src/doc-bundle/*.js',
-]
-const nodeMJS = [ // node, latest, module
-    'nodes/**/*.mjs',
-    'eslint.config.mjs',
-]
+// Folder/file lists - eslint flat config is WEIRD!
+// You have to override the top-level config (e.g. **/*.js) and then exclude the folders/files you don't want.
 
-// General rules
-// const general = {
-//     plugins: {
-//         '@stylistic': stylistic,
-//     },
-//     rules: {
-//         'jsdoc/check-alignment': 'warn',
-//         // "jsdoc/check-indentation": ["warn", {"excludeTags":['example', 'description']}],
-//         'jsdoc/check-indentation': 'off',
-//         'jsdoc/check-param-names': 'warn',
-//         'jsdoc/check-tag-names': ['warn', {
-//             definedTags: ['typicalname'],
-//         }],
-//         'jsdoc/multiline-blocks': ['error', {
-//             noZeroLineText: false,
-//         }],
-//         'jsdoc/no-undefined-types': ['error', {
-//             'definedTypes': ['NodeListOf'],
-//         }],
-//         'jsdoc/tag-lines': 'off',
-
-//         '@stylistic/comma-dangle': ['error', {
-//             'arrays': 'only-multiline',
-//             'objects': 'always',
-//             'imports': 'never',
-//             'exports': 'always-multiline',
-//             'functions': 'never',
-//             'importAttributes': 'never',
-//             'dynamicImports': 'never',
-//         }],
-//         '@stylistic/eol-last': ['error', 'always'],
-//         '@stylistic/indent': ['error', 4, {
-//             'SwitchCase': 1,
-//         }],
-//         '@stylistic/linebreak-style': ['error', 'unix'],
-//         '@stylistic/lines-between-class-members': ['error', 'always'],
-//         '@stylistic/newline-per-chained-call': ['error', {
-//             'ignoreChainWithDepth': 2,
-//         }],
-//         '@stylistic/no-confusing-arrow': 'error',
-//         '@stylistic/no-extra-semi': 'error',
-//         '@stylistic/no-mixed-spaces-and-tabs': 'error',
-//         '@stylistic/no-trailing-spaces': 'error',
-//         '@stylistic/semi': ['error', 'never'],
-//         '@stylistic/quotes': ['error', 'single', {
-//             'avoidEscape': true,
-//             'allowTemplateLiterals': 'always',
-//         }],
-
-//         'new-cap': 'error',
-//         'prefer-const': 'error',
-//         'no-else-return': 'error',
-//         'no-empty': ['error', {
-//             allowEmptyCatch: true,
-//         }],
-//         'no-unused-vars': 'off',
-//         'no-useless-escape': 'off',
-//         'no-var': 'warn',
-//     },
-//     settings: {
-//         jsdoc: {
-//             mode: 'jsdoc',
-//         },
-//     },
-//     ignores: ['tsconfig.json'],
-// }
+// Shared rules
+const jsdocRules = {
+    'jsdoc/check-alignment': 'off',
+    // "jsdoc/check-indentation": ["warn", {"excludeTags":['example', 'description']}],
+    'jsdoc/check-indentation': 'off',
+    'jsdoc/check-param-names': 'warn',
+    'jsdoc/check-tag-names': ['warn', {
+        definedTags: ['typicalname', 'element', 'memberOf', 'slot', 'csspart'],
+    }],
+    'jsdoc/multiline-blocks': ['error', {
+        noZeroLineText: false,
+    }],
+    'jsdoc/no-multi-asterisk': 'off',
+    'jsdoc/no-undefined-types': ['error', {
+        'definedTypes': ['NodeListOf', 'ProxyHandler'],
+    }],
+    'jsdoc/tag-lines': 'off',
+}
+const stylisticRules = {
+    '@stylistic/js/brace-style': ["error", "1tbs", { "allowSingleLine": true }],
+    '@stylistic/comma-dangle': ['error', {
+        'arrays': 'only-multiline',
+        'objects': 'always',
+        'imports': 'never',
+        'exports': 'always-multiline',
+        'functions': 'never',
+        'importAttributes': 'never',
+        'dynamicImports': 'never',
+    }],
+    '@stylistic/eol-last': ['error', 'always'],
+    '@stylistic/indent': ['error', 4, {
+        'SwitchCase': 1,
+    }],
+    '@stylistic/linebreak-style': ['error', 'unix'],
+    '@stylistic/lines-between-class-members': 'off',
+    '@stylistic/newline-per-chained-call': ['error', {
+        'ignoreChainWithDepth': 2,
+    }],
+    '@stylistic/no-confusing-arrow': 'error',
+    '@stylistic/no-extra-semi': 'error',
+    '@stylistic/no-mixed-spaces-and-tabs': 'error',
+    '@stylistic/no-trailing-spaces': 'error',
+    '@stylistic/semi': ['error', 'never'],
+    '@stylistic/space-before-function-paren': 'off',
+    '@stylistic/spaced-comment': ['error', 'always', {
+        'line': {
+            'exceptions': ['*', '#region', '#endregion', ],
+        },
+        'block': {
+            'exceptions': ['*', ],
+        },
+    }],
+    '@stylistic/space-in-parens': 'off',
+    '@stylistic/quotes': ['error', 'single', {
+        'avoidEscape': true,
+        'allowTemplateLiterals': 'always',
+    }],
+}
+const generalRules = {
+    'new-cap': 'error',
+    'no-else-return': 'error',
+    'no-empty': ['error', {
+        allowEmptyCatch: true,
+    }],
+    'no-unused-vars': 'off',
+    'no-useless-escape': 'off',
+    'no-var': 'warn',
+    'prefer-const': 'error',
+}
 
 /** @type {import('eslint').Linter.Config[]} */
-const conf = [
-    js.configs.recommended,
-    jsdoc.configs['flat/recommended'],
-    pluginPromise.configs['flat/recommended'],
-    pluginImport.flatConfigs.recommended,
-
-    // {
-    //     ignores: ['tsconfig.json'],
-    // },
-
-    // General
-    {
-        plugins: {
-            '@stylistic': stylistic,
-        },
-        rules: {
-            'jsdoc/check-alignment': 'off',
-            // "jsdoc/check-indentation": ["warn", {"excludeTags":['example', 'description']}],
-            'jsdoc/check-indentation': 'off',
-            'jsdoc/check-param-names': 'warn',
-            'jsdoc/check-tag-names': ['warn', {
-                definedTags: ['typicalname', 'element', 'memberOf', 'slot', 'csspart'],
-            }],
-            'jsdoc/multiline-blocks': ['error', {
-                noZeroLineText: false,
-            }],
-            'jsdoc/no-multi-asterisk': 'off',
-            'jsdoc/no-undefined-types': ['error', {
-                'definedTypes': ['NodeListOf', 'ProxyHandler'],
-            }],
-            'jsdoc/tag-lines': 'off',
-
-            '@stylistic/comma-dangle': ['error', {
-                'arrays': 'only-multiline',
-                'objects': 'always',
-                'imports': 'never',
-                'exports': 'always-multiline',
-                'functions': 'never',
-                'importAttributes': 'never',
-                'dynamicImports': 'never',
-            }],
-            '@stylistic/eol-last': ['error', 'always'],
-            '@stylistic/indent': ['error', 4, {
-                'SwitchCase': 1,
-            }],
-            '@stylistic/linebreak-style': ['error', 'unix'],
-            '@stylistic/lines-between-class-members': 'off',
-            '@stylistic/newline-per-chained-call': ['error', {
-                'ignoreChainWithDepth': 2,
-            }],
-            '@stylistic/no-confusing-arrow': 'error',
-            '@stylistic/no-extra-semi': 'error',
-            '@stylistic/no-mixed-spaces-and-tabs': 'error',
-            '@stylistic/no-trailing-spaces': 'error',
-            '@stylistic/semi': ['error', 'never'],
-            '@stylistic/quotes': ['error', 'single', {
-                'avoidEscape': true,
-                'allowTemplateLiterals': 'always',
-            }],
-
-            'new-cap': 'error',
-            'no-else-return': 'error',
-            'no-empty': ['error', {
-                allowEmptyCatch: true,
-            }],
-            'no-unused-vars': 'off',
-            'no-useless-escape': 'off',
-            'no-var': 'warn',
-            'prefer-const': 'error',
-        },
-        settings: {
-            jsdoc: {
-                mode: 'jsdoc',
-            },
-        },
-        // languageOptions: {
-        //     sourceType: 'commonjs',
-        //     ecmaVersion: 'latest',
-        // },
-    },
+export default defineConfig([
 
     // Browser (ES2019) script, no-build
     {
-        files: browserCJS,
-        // ...general,
-        ...js.configs.browser,
+        files: ['**/*.{js,cjs}'],
+        ignores: ['nodes/**/*.{js,cjs}'],
         languageOptions: {
             sourceType: 'script',
             ecmaVersion: 2019,
             globals: {
                 ...globals.browser,
-                window: 'writable', // allow setting window global properties
+                // window: 'writable', // allow setting window global properties
                 jQuery: 'readonly',
                 RED: 'readonly',
                 uibuilder: 'writable',
-                console: 'readonly',
+                '$': 'readonly',
+                '$$': 'readonly',
+                // console: 'readonly',
             },
         },
-        // rules: {
-        //     'no-empty': ['error', { 'allowEmptyCatch': true }],
-        // },
+        linterOptions: {
+			reportUnusedInlineConfigs: "error",
+		},
+        plugins: {
+            'js': js,
+            'pluginPromise': pluginPromise,
+            'jsdoc': jsdoc,
+            '@stylistic': stylistic,
+        },
+        extends: [
+            js.configs.recommended,
+            jsdoc.configs['flat/recommended'],
+            stylistic.configs.recommended,
+            pluginPromise.configs['flat/recommended'],
+        ],
+        settings: {
+            jsdoc: { mode: 'jsdoc', },
+        },
+        rules: {
+            ...jsdocRules,
+            ...stylisticRules,
+            ...generalRules,
+            // 'no-empty': ['error', { 'allowEmptyCatch': true }],
+        },
     },
 
     // Browser (Latest) ESM, ESBUILD
     {
-        files: browserMJS,
-        // ...general,
-        ...js.configs.browser,
+        files: ['**/*.mjs'],
+        ignores: ['nodes/**/*.{js,cjs,mjs}'],
+        // ...pluginImport.flatConfigs.recommended,
         languageOptions: {
             sourceType: 'module',
             ecmaVersion: 'latest',
             globals: {
                 ...globals.browser,
-                window: 'writable', // allow setting window global properties
-                globalThis: 'writable',
+                // window: 'writable', // allow setting window global properties
                 jQuery: 'readonly',
                 RED: 'readonly',
                 uibuilder: 'writable',
-                console: 'readonly',
+                '$': 'readonly',
+                '$$': 'readonly',
+                // console: 'readonly',
             },
         },
-        // rules: {
-        //     'no-empty': 'off',
-        // },
+        linterOptions: {
+			reportUnusedInlineConfigs: "error",
+		},
+        plugins: {
+            'js': js,
+            'pluginPromise': pluginPromise,
+            'pluginImport': pluginImport,
+            'jsdoc': jsdoc,
+            '@stylistic': stylistic,
+        },
+        extends: [
+            js.configs.recommended,
+            jsdoc.configs['flat/recommended'],
+            stylistic.configs.recommended,
+            pluginPromise.configs['flat/recommended'],
+            pluginImport.flatConfigs.recommended,
+        ],
+        settings: {
+            jsdoc: { mode: 'jsdoc', },
+        },
+        rules: {
+            ...jsdocRules,
+            ...stylisticRules,
+            ...generalRules,
+            // 'no-empty': ['error', { 'allowEmptyCatch': true }],
+        },
+
     },
 
     // Node.js (v18) CommonJS, no-build
     {
-        files: nodeCJS,
-        ...node.configs['flat/recommended-script'],
+        // files: nodeCJS,
+        files: ['**/*.{js,cjs}', ],
+        ignores: ['resources/*.{js,cjs}'],
         languageOptions: {
             sourceType: 'commonjs',
             // Will be overridden by the n plugin which detects the correct node.js version from package.json
@@ -257,23 +201,41 @@ const conf = [
             // Node.js globals are provided by the n plugin
             // globals: globals.browser,
         },
-        rules: {
-            'n/no-missing-import': 'error',
-            'n/no-process-exit': 'warn',
+        linterOptions: {
+			reportUnusedInlineConfigs: "error",
+		},
+        plugins: {
+            'js': js,
+            'pluginImport': pluginImport,
+            'pluginPromise': pluginPromise,
+            'jsdoc': jsdoc,
+            '@stylistic': stylistic,
+            'n': node, // <= n/node
         },
-        // Better to pick up from package.json unless needing to override
-        // settings: {
-        //     node: {
-        //         version: '18.0.0',
-        //     }
-        // },
+        extends: [
+            js.configs.recommended,
+            jsdoc.configs['flat/recommended'],
+            stylistic.configs.recommended,
+            pluginPromise.configs['flat/recommended'],
+            node.configs['flat/recommended-script'], // <= script/commonjs
+        ],
+        settings: {
+            jsdoc: { mode: 'jsdoc', },
+            // Better to pick up from package.json unless needing to override
+            // node: { version: '18.0.0', },
+        },
+        rules: {
+            ...jsdocRules,
+            ...stylisticRules,
+            ...generalRules,
+        },
     },
 
     // Node.js (LTS) ESM, ESBUILD
     {
-        files: nodeMJS,
-        // ...general,
-        ...node.configs['flat/recommended-module'],
+        // files: nodeMJS,
+        files: ['**/*.mjs', ],
+        ignores: ['resources/*.{js,cjs,mjs}', ],
         languageOptions: {
             sourceType: 'module',
             // Will be overridden by the n plugin which detects the correct node.js version from package.json
@@ -281,19 +243,38 @@ const conf = [
             // Node.js globals are provided by the n plugin
             // globals: globals.browser,
         },
+        linterOptions: {
+			reportUnusedInlineConfigs: "error",
+		},
+        plugins: {
+            'js': js,
+            'pluginImport': pluginImport,
+            'pluginPromise': pluginPromise,
+            'jsdoc': jsdoc,
+            '@stylistic': stylistic,
+            'n': node, // <= n/node
+        },
+        extends: [
+            js.configs.recommended,
+            jsdoc.configs['flat/recommended'],
+            stylistic.configs.recommended,
+            pluginPromise.configs['flat/recommended'],
+            pluginImport.flatConfigs.recommended,
+            node.configs['flat/recommended-module'], // <= module/ESM
+        ],
+        settings: {
+            jsdoc: { mode: 'jsdoc', },
+            // Override for node.js current LTS (assuming the use of ESBUILD)
+            // Better to pick up from package.json unless needing to override
+            node: { version: 'lts', },
+        },
         rules: {
+            ...jsdocRules,
+            ...stylisticRules,
+            ...generalRules,
             'n/no-unsupported-features/es-syntax': 'off', // Allow all modern ESM features
             'n/no-missing-import': 'error',
             'n/no-process-exit': 'warn',
         },
-        // Better to pick up from package.json unless needing to override
-        // Override for node.js current LTS (assuming the use of ESBUILD)
-        settings: {
-            node: {
-                version: 'lts', // Use latest LTS version
-            },
-        },
     },
-]
-
-export default conf
+])
