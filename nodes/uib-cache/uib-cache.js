8ca65f28aaaea810e602dcc966a317d6fe56239b
Julian Knight <1591850+TotallyInformation@users.noreply.github.com>
2025-06-13 16:40:29 +0100

    Fixed entry count=0 problem

--- nodes/uib-cache/uib-cache.js
+++ nodes/uib-cache/uib-cache.js
@@ -3,7 +3,7 @@
  *  If it receives a cache-replay control msg, it dumps the cache.
  *  If it receives a cache-empty control msg, it empties the cache.
  *
- * Copyright (c) 2022-2024 Julian Knight (Totally Information)
+ * Copyright (c) 2022-2025 Julian Knight (Totally Information)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -27,8 +27,6 @@
  * @typedef {import('../../typedefs.js').cacheNode1} cacheNode
  * typedef {import('../typedefs.js').myNode} myNode
  */
-
-// #region ----- Module level variables ---- //
 
 /** Main (module) variables - acts as a configuration object
  *  that can easily be passed around.
@@ -58,6 +56,7 @@ function setNodeStatus(node) {
  * @param {runtimeNode & cacheNode} node Reference to node instance
  */
 function trimCacheAll(node) {
+    if (node.num === 0) return // 0 = infinite, so no need to trim
     Object.keys(node.cache).forEach( (key) => {
         const msgs = node.cache[key]
         // See if the array is now too long - if so, slice it down to size
@@ -78,22 +77,26 @@ function addToCache(msg, node) {
     if (mod.RED === null) return
     if (node.cacheKey === undefined) return
 
+    const cacheKey = node.cacheKey
+
     // If msg[<cacheKey>] doesn't exist (or is an empty string), do not process
-    if ( !msg[node.cacheKey] ) return
+    if ( !msg[cacheKey] ) return
+
+    const cacheKeyValue = msg[cacheKey]
 
     // If this is a new property value in the stored variable, create empty array
-    if ( !node.cache[msg[node.cacheKey]] ) node.cache[msg[node.cacheKey]] = []
+    if ( !node.cache[cacheKeyValue] ) node.cache[cacheKeyValue] = []
 
     // HAS to be a CLONE to avoid downstream changes impacting cache
     const clone = mod.RED.util.cloneMessage(msg)
     delete clone._msgid
 
-    // Add a new entry to the array -
-    node.cache[clone[node.cacheKey]].push(clone)
+    // Add a new entry to the array
+    node.cache[cacheKeyValue].push(clone)
 
-    // See if the array is now too long - if so, slice it down to size
-    if ( node.cache[clone[node.cacheKey]].length > node.num ) {
-        node.cache[clone[node.cacheKey]] = node.cache[clone[node.cacheKey]].slice( node.cache[clone[node.cacheKey]].length - node.num )
+    // See if the array is now too long - if so, slice it down to size (unless num=0)
+    if ( node.num !== 0 && node.cache[cacheKeyValue].length > node.num ) {
+        node.cache[cacheKeyValue] = node.cache[cacheKeyValue].slice( node.cache[cacheKeyValue].length - node.num )
     }
 
     // Save the cache
@@ -154,6 +157,91 @@ function sendCache(send, node, msg) {
 // TODO: Editor option to to ignore replay socket id
 // TODO: Editor options to clear the cache, clear first n, clear last n entries
 
+/** 1) Complete module definition for our Node. This is where things actually start.
+ * @param {runtimeRED} RED The Node-RED runtime object
+ */
+function UibCache(RED) {
+    // As a module-level named function, it will inherit `mod` and other module-level variables
+
+    // Save a reference to the RED runtime for convenience
+    mod.RED = RED
+
+    /** Register a new instance of the specified node type (2)
+     *
+     */
+    RED.nodes.registerType(mod.nodeName, nodeInstance)
+}
+
+/** 2) This is run when an actual instance of our node is committed to a flow
+ * type {function(this:runtimeNode&senderNode, runtimeNodeConfig & senderNode):void}
+ * @param {runtimeNodeConfig & cacheNode} config The Node-RED node instance config object
+ * @this {runtimeNode & cacheNode}
+ */
+function nodeInstance(config) {
+    // As a module-level named function, it will inherit `mod` and other module-level variables
+
+    // If you need it - which you will here - or just use mod.RED if you prefer:
+    const RED = mod.RED
+    if (RED === null) return
+
+    // Create the node instance - `this` can only be referenced AFTER here
+    RED.nodes.createNode(this, config)
+
+    /** Transfer config items from the Editor panel to the runtime */
+    this.name = config.name
+    this.cacheall = config.cacheall
+    this.cacheKey = config.cacheKey ?? 'topic'
+    this.newcache = config.newcache ?? true
+    this.num = Number(config.num) // zero is unlimited cache
+    this.storeName = config.storeName ?? 'default'
+    this.storeContext = config.storeContext ?? 'context'
+    this.varName = config.varName ?? 'uib_cache'
+
+    if (Number.isNaN(this.num) || this.num < 0) {
+        this.num = 1
+    }
+
+    // Show if anything in the cache
+    setNodeStatus(this)
+
+    // Get ref to this node's context store or the flow/global stores as needed
+    let context = this.context()
+    if ( this.storeContext !== 'context') {
+        context = context[this.storeContext]
+    }
+    this.getC = context.get
+    this.setC = context.set
+
+    // Get the cache or initialise it if new
+    this.cache = this.getC(this.varName, this.storeName) || {}
+    // Note that the cache is written back in addToCache and clearCache
+
+    if (this.cacheall === true) {
+        this.cacheKey = undefined
+    }
+
+    trimCacheAll(this)
+
+    /** Handle incoming msg's - note that the handler fn inherits `this` */
+    this.on('input', inputMsgHandler)
+
+    /** Put things here if you need to do anything when a node instance is removed
+     * Or if Node-RED is shutting down.
+     * Note the use of an arrow function, ensures that the function keeps the
+     * same `this` context and so has access to all of the node instance properties.
+     */
+    this.on('close', (removed, done) => {
+        done()
+    })
+
+    /** Properties of `this`
+     * Methods: updateWires(wires), context(), on(event,callback), emit(event,...args), removeListener(name,listener), removeAllListeners(name), close(removed)
+     *          send(msg), receive(msg), log(msg), warn(msg), error(logMessage,msg), debug(msg), trace(msg), metric(eventname, msg, metricValue), status(status)
+     * Other: credentials, id, type, z, wires, x, y
+     * + any props added manually from config, typically at least name and topic
+     */
+}
+
 /** 3) Run whenever a node instance receives a new input msg
  * NOTE: `this` context is still the parent (nodeInstance).
  * See https://nodered.org/blog/2019/09/20/node-done
@@ -202,89 +290,6 @@ function inputMsgHandler(msg, send, done) {
     // We are done
     // done()
 } // ----- end of inputMsgHandler ----- //
-
-/** 2) This is run when an actual instance of our node is committed to a flow
- * type {function(this:runtimeNode&senderNode, runtimeNodeConfig & senderNode):void}
- * @param {runtimeNodeConfig & cacheNode} config The Node-RED node instance config object
- * @this {runtimeNode & cacheNode}
- */
-function nodeInstance(config) {
-    // As a module-level named function, it will inherit `mod` and other module-level variables
-
-    // If you need it - which you will here - or just use mod.RED if you prefer:
-    const RED = mod.RED
-    if (RED === null) return
-
-    // Create the node instance - `this` can only be referenced AFTER here
-    RED.nodes.createNode(this, config)
-
-    /** Transfer config items from the Editor panel to the runtime */
-    this.name = config.name
-    this.cacheall = config.cacheall
-    this.cacheKey = config.cacheKey || 'topic'
-    this.newcache = config.newcache ?? true
-    this.num = config.num ?? 1 // zero is unlimited cache
-    this.storeName = config.storeName || 'default'
-    this.storeContext = config.storeContext || 'context'
-    this.varName = config.varName || 'uib_cache'
-
-    // Show if anything in the cache
-    setNodeStatus(this)
-
-    // Get ref to this node's context store or the flow/global stores as needed
-    let context = this.context()
-    if ( this.storeContext !== 'context') {
-        context = context[this.storeContext]
-    }
-    this.getC = context.get
-    this.setC = context.set
-
-    // Get the cache or initialise it if new
-    this.cache = this.getC(this.varName, this.storeName) || {}
-    // Note that the cache is written back in addToCache and clearCache
-
-    if (this.cacheall === true) {
-        this.cacheKey = undefined
-    }
-
-    trimCacheAll(this)
-
-    /** Handle incoming msg's - note that the handler fn inherits `this` */
-    this.on('input', inputMsgHandler)
-
-    /** Put things here if you need to do anything when a node instance is removed
-     * Or if Node-RED is shutting down.
-     * Note the use of an arrow function, ensures that the function keeps the
-     * same `this` context and so has access to all of the node instance properties.
-     */
-    this.on('close', (removed, done) => {
-        done()
-    })
-
-    /** Properties of `this`
-     * Methods: updateWires(wires), context(), on(event,callback), emit(event,...args), removeListener(name,listener), removeAllListeners(name), close(removed)
-     *          send(msg), receive(msg), log(msg), warn(msg), error(logMessage,msg), debug(msg), trace(msg), metric(eventname, msg, metricValue), status(status)
-     * Other: credentials, id, type, z, wires, x, y
-     * + any props added manually from config, typically at least name and topic
-     */
-}
-
-// #endregion ----- Module-level support functions ----- //
-
-/** 1) Complete module definition for our Node. This is where things actually start.
- * @param {runtimeRED} RED The Node-RED runtime object
- */
-function UibCache(RED) {
-    // As a module-level named function, it will inherit `mod` and other module-level variables
-
-    // Save a reference to the RED runtime for convenience
-    mod.RED = RED
-
-    /** Register a new instance of the specified node type (2)
-     *
-     */
-    RED.nodes.registerType(mod.nodeName, nodeInstance)
-}
 
 // Export the module definition (1), this is consumed by Node-RED on startup.
 module.exports = UibCache
