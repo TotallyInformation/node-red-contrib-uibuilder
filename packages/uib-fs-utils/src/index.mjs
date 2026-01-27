b5484d08ba637f0cc99dd06042b627afed7c2ca3
Julian Knight <1591850+TotallyInformation@users.noreply.github.com>
2026-01-27 17:37:06 +0000

    New FS utils sub-package to enable chokidar extended watch

---
+++ packages/uib-fs-utils/src/index.mjs
@@ -0,0 +1,9 @@
+/** Internal Filing System utilities for uibuilder - bundles chokidar
+ * Re-exports dependencies for use in production without adding to main package.json
+ */
+
+import chokidar from 'chokidar'
+
+export {
+    chokidar,
+}
