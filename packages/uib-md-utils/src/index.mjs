b79044fc3bc74a8126987183755e2a61d28d5ff8
Julian Knight <1591850+TotallyInformation@users.noreply.github.com>
2026-02-02 16:35:14 +0000

    Tidy

--- packages/uib-md-utils/src/index.mjs
+++ packages/uib-md-utils/src/index.mjs
@@ -69,58 +69,9 @@ const parser = md
         }
     )
 
-/** Custom heading ID renderer for marked to allow {#custom-id} syntax in headings
- * See: https://github.com/markedjs/marked-custom-heading-id
- * @returns {object} marked renderer override
- */
-function customHeadingId() {
-    return {
-        useNewRenderer: true,
-        renderer: {
-            // image(src, title, alt) {
-            //     console.log('✅✅ Marked image:', {src, alt, title})
-            //     const headingIdRegex = /(?: +|^)\{([a-z][\w-]*)\}(?: +|$)/i
-            //     const hasId = src.raw.match(headingIdRegex)
-            //     if (!hasId) {
-            //         // Remove
-            //     }
-            //     return false // fallback to original image renderer
-            // },
-            heading(text, depth) {
-                if (typeof text !== 'string') {
-                    depth = text.depth
-                    text = text.text
-                }
-                const headingIdRegex = /(?: +|^)\{#([a-z][\w-]*)\}(?: +|$)/i
-                const hasId = text.match(headingIdRegex)
-                let id
-                if (hasId) {
-                    // Use the custom ID
-                    id = hasId[1]
-                } else {
-                    // Try to use the heading text as ID (after sanitizing)
-                    id = text.toLowerCase()
-                        .replace(/[^\w]+/g, '-')
-                        .replace(/^-+|-+$/g, '')
-                        .trim()
-                }
-                // Remove the {#custom-id} from the text
-                text = text.replace(headingIdRegex, '').trim()
-                return `<h${depth} id="${id}">${text}<a href="#${id}"> 🔗</a></h${depth}>\n`
-            },
-        },
-    }
-}
-
-// marked.use(
-//     markedAlert(),
-//     customHeadingId()
-// )
-
 const mdParse = md.render.bind(md)
 
 export {
-    // marked as mdParse,
     fm,
     mdParse,
 }
