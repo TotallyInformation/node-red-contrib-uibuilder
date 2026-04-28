ceb226b40cb28cf5d7b7c9331e17e5efb485e63b
Julian Knight <1591850+TotallyInformation@users.noreply.github.com>
2026-04-28 14:52:12 +0100

    Not in use

--- front-end/utils/markweb-nav.js
+++
@@ -1,9 +0,0 @@
-/** Markweb Navigation - Burger menu toggle on scroll past header
- * Handles collapsing the horizontal nav to a burger icon when scrolled past the header.
- * @module markweb-nav
- */
-;(function() {
-    'use strict'
-
-
-})()
