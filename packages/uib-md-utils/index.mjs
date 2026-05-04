3d774396e119adfa5a2947973abcbe7312fa22c3
Julian Knight <1591850+TotallyInformation@users.noreply.github.com>
2026-05-04 16:39:28 +0100

    Build now forces inclusion of require function

--- packages/uib-md-utils/index.mjs
+++ packages/uib-md-utils/index.mjs
@@ -1,3 +1,6 @@
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+
 var __create = Object.create;
 var __defProp = Object.defineProperty;
 var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
